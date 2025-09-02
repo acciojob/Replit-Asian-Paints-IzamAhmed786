b68393f81f1210b2170199c36c1d4f4d3a9aee74
IzamAhmed786 <izamahmed786@gmail.com>
2025-09-02 10:40:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+    const changeBtn = document.getElementById("change_button");
+    const resetBtn = document.getElementById("Reset");
+    const gridItems = document.querySelectorAll(".grid-item");
+function resetGrid() {
+      gridItems.forEach(item => {
+        item.style.backgroundColor = "transparent";
+      });
+    }
+changeBtn.addEventListener("click", () => {
+      const blockId = document.getElementById("block_id").value;
+      const color = document.getElementById("colour_id").value;
+
+	resetGrid();
+
+	const block = document.getElementById(blockId);
+      if (block && color) {
+        block.style.backgroundColor = color;
+      } else {
+        alert("Please enter a valid Block ID (1–9) and a color.");
+      }
+    });
+    resetBtn.addEventListener("click", resetGrid);
